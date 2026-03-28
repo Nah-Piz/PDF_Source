45322ab0ca66039863b938fda517a39d88e7f879
Onapa Brian <brianonapa2k03@gmail.com>
2026-03-28 23:07:10 +0300

    frontend files not loading on production correction

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({ extended: true }));
 
 const __dirname = path.resolve();
 
-app.use(express.static(path.join(__dirname, "app", "dist", "index.html")));
+app.use(express.static(path.join(__dirname, "app", "dist")));
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "app", "dist", "index.html"));
